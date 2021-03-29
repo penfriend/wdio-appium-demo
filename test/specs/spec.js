63cb60709aa796d50aa188078dd7239f85842257
Diana Holik <DHolik@slb.com>
2021-03-29 09:50:21 +0300

    different strategies are used in finding locators

--- test/specs/spec.js
+++ test/specs/spec.js
@@ -3,6 +3,14 @@ describe ('basic test', () => {
 
   it('should open the app', () => {
     console.log('\nIt works!!!\n');
+    browser.debug();
+    const selector = 'new UiSelector().text("Continue")).className("com.android.permissioncontroller")';
+    browser.click('android=' + selector);
+    $(~Accessibility).click();
+    const customView = '//android.widget.TextView[@content-desc="Custom View"]';
+    browser.click(customView);
+
+
   });
 
 });
